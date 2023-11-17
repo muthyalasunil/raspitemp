6a2c85aa377171ed2b7c92a732a70a49ef072c77
muthyalasunil <muthyalasunil@gmail.com>
2023-11-17 20:44:01 +0530

    added analyse to script

--- rundht.sh
+++ rundht.sh
@@ -2,4 +2,4 @@
 cd /home/admin/Adafruit_Python_DHT/Adafruit_DHT
 git pull -f
 /usr/bin/python /home/admin/Adafruit_Python_DHT/Adafruit_DHT/dhttest.py >> /home/admin/logs/$(date +'%Y-%m-%d').log
-
+/usr/bin/python /home/admin/Adafruit_Python_DHT/Adafruit_DHT/analyse.py | tail -10 > /home/admin/logs/report.txt
