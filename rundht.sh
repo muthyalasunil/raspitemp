5aa7e91af48b6e1762457eebb29c1efca089f789
muthyalasunil <muthyalasunil@gmail.com>
2023-11-18 11:28:56 +0530

    ip

--- rundht.sh
+++ rundht.sh
@@ -3,6 +3,7 @@ cd /home/admin/Adafruit_Python_DHT/Adafruit_DHT
 git checkout
 /usr/bin/python /home/admin/Adafruit_Python_DHT/Adafruit_DHT/dhttest.py >> /home/admin/logs/$(date +'%Y-%m-%d').log
 /usr/bin/python /home/admin/Adafruit_Python_DHT/Adafruit_DHT/analyse.py | tail -10 > /home/admin/logs/report.txt
+hostname -I >> /home/admin/logs/report.txt
 mail -s "report" muthyalasunil@yahoo.com < /home/admin/logs/report.txt
  
 
