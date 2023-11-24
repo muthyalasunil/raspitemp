cee13c3de7aad2cd52b7258958b980dff3fae1a7
muthyalasunil <muthyalasunil@gmail.com>
2023-11-24 19:47:42 +0530

    mail_to fix

--- rundht.sh
+++ rundht.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+source /home/admin/.bashrc
+
 /usr/bin/python /home/admin/Adafruit_Python_DHT/Adafruit_DHT/dhttest.py >> /home/admin/logs/$(date +'%d').log
 /usr/bin/python /home/admin/Adafruit_Python_DHT/Adafruit_DHT/analyse.py | tail -10 > /home/admin/logs/report.txt
 hostname -I >> /home/admin/logs/report.txt
 
 if [ "$(ping -c1 8.8.8.8)" ]
 then
-	mail -s "$(echo $mail_subject)" "$(echo $mail_to_list)" < /home/admin/logs/report.txt
+
+	mail -s "$(cat ~/mail_subject)" "$(cat ~/mail_to_list)" < /home/admin/logs/report.txt
 fi 
 
