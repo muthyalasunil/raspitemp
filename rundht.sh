b44995818d472528222374178e01ed8e3ffdd635
muthyalasunil <muthyalasunil@gmail.com>
2023-11-24 16:36:49 +0530

    check wifi

--- rundht.sh
+++ rundht.sh
@@ -2,6 +2,9 @@
 /usr/bin/python /home/admin/Adafruit_Python_DHT/Adafruit_DHT/dhttest.py >> /home/admin/logs/$(date +'%d').log
 /usr/bin/python /home/admin/Adafruit_Python_DHT/Adafruit_DHT/analyse.py | tail -10 > /home/admin/logs/report.txt
 hostname -I >> /home/admin/logs/report.txt
-mail -s "report" muthyalasunil@yahoo.com < /home/admin/logs/report.txt
- 
+
+if [ "$(ping -c1 8.8.8.8)" ]
+then
+	mail -s "report" muthyalasunil@yahoo.com < /home/admin/logs/report.txt
+fi 
 
