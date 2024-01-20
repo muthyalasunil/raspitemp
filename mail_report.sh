0361f33f3d4c273abb11a4d00a6c4042485877c6
muthyalasunil <muthyalasunil@gmail.com>
2024-01-20 22:21:56 +0530

    fixed mail script

--- mail_report.sh
+++ mail_report.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 
-if [ "$iwgetid" == "Raspiwifi" ]
+essid=`iwgetid | cut -f 6 -s -d" " | cut -f 2 -s -d:`
+#echo $essid
+if [ $essid="Raspiwifi" ]
 then
-	echo "Mailing on Raspiwif.."
-	/usr/bin/python /home/admin/Adafruit_Python_DHT/Adafruit_DHT/analyse.py | tail -10 > /home/admin/logs/report.txt
-	hostname -I >> /home/admin/logs/report.txt
-	mail -s "$(cat ~/mail_subject)" "$(cat ~/mail_to_list)" < /home/admin/logs/report.txt
+	echo "Mailing on $essid.."
+	/usr/bin/python ~/raspitemp/analyse.py | tail -10 > ~/raspitemp/logs/report.txt
+	#hostname -I >> ~/raspitemp/logs/report.txt
+	#echo "$(cat ~/raspitemp/mail_subject) $essid"
+	mail -s "$(cat ~/raspitemp/mail_subject) $essid" "$(cat ~/raspitemp/mail_to_list)" < ~/raspitemp/logs/report.txt
 fi 
 
